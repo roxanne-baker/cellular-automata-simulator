12f62f8962aff0b25e765aa4bf8b0ac2e1e07d13
Roxanne Baker <reb36@duke.edu>
2016-02-17 05:03:03 -0500

    Add master code to personal branch

--- src/SugarScapeCell.java
+++ src/SugarScapeCell.java
@@ -4,20 +4,20 @@ import javafx.scene.shape.Shape;
 
 public class SugarScapeCell extends Cell{
 
-	double sugarAmount;
-	double sugarMax;
-	int sugarGrowBackRate;
-	int sugarGrowBackInterval = 0;
-	List<SugarScapeCell> neighbors;
-	SugarAgent agent;
+	private double sugarAmount;
+	private double sugarMax;
+	private int sugarGrowBackRate;
+	private int sugarGrowBackInterval = 0;
+	private List<SugarScapeCell> neighbors;
+	private SugarAgent agent;
 	
-	int row;
-	int col;
-	
+	private int row;
+	private int col;
+
 	public SugarScapeCell(Shape shape, int row, int col, int sugarMax, int sugarGrowBackRate) {
 		super(shape);
 		this.sugarMax = sugarMax;
-		sugarAmount = sugarMax;
+		setSugarAmount(sugarMax);
 		this.sugarGrowBackRate = sugarGrowBackRate;
 		this.row = row;
 		this.col = col;
@@ -26,12 +26,42 @@ public class SugarScapeCell extends Cell{
 	public void update() {
 		sugarGrowBackInterval++;
 		if (sugarGrowBackInterval >= sugarGrowBackRate) {
-			sugarAmount = Math.min(sugarAmount+sugarGrowBackRate, sugarMax);
+			setSugarAmount(Math.min(getSugarAmount()+sugarGrowBackRate, sugarMax));
 			sugarGrowBackInterval = 0;
 		}
 	}
-	
 
+	public SugarAgent getAgent() {
+		return agent;
+	}
+
+	public void setAgent(SugarAgent agent) {
+		this.agent = agent;
+	}
+
+	public List<SugarScapeCell> getNeighbors() {
+		return neighbors;
+	}
+	
+	public double getSugarRatio() {
+		return getSugarAmount()/sugarMax;
+	}
+	
+	public int getRow() {
+		return row;
+	}
+
+	public int getCol() {
+		return col;
+	}
+
+	public double getSugarAmount() {
+		return sugarAmount;
+	}
+
+	public void setSugarAmount(double sugarAmount) {
+		this.sugarAmount = sugarAmount;
+	}
 	
 	
 	
